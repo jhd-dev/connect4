fe28c49b91ef2996e114d3063b5ce34710d182fa
Programmer Account <programmer@hs-lab-2.local>
2017-08-29 12:20:20 -0400

    object-oriented style

--- connect4.py
+++ connect4.py
@@ -1,35 +1,42 @@
-board = [[0 for j in range(7)] for i in range(8)]
-game_running = True
-current_player = 1
-win_combinations = [
-	[(0, 0), (1, 0), (2, 0), (3, 0)],
-	[(0, 0), (0, 1), (0, 2), (0, 3)],
-	[(0, 0), (1, 1), (2, 2), (3, 3)]
-]
+class Connect4:
 
-while game_running:
-	switch_player()
-	get_player_input()
-	display_board()
-	check_for_victory()
+	def setup(self):
+		self.board = [[0 for j in range(7)] for i in range(8)]
+		self.game_running = True
+		self.current_player = 1
+		self.win_combinations = [
+			[(0, 0), (1, 0), (2, 0), (3, 0)],
+			[(0, 0), (0, 1), (0, 2), (0, 3)],
+			[(0, 0), (1, 1), (2, 2), (3, 3)]
+		]
+		return self
 
-def switch_player():
-	if current_player == 1:
-		current_player = 2
-	else:
-		current_player = 1
+	def run(self):
+		while self.game_running:
+			self.switch_player()
+			self.get_player_input()
+			self.display_board()
+			self.check_for_victory()
 
-def get_player_input():
-	column_number = raw_input('Player ' + current_player + '\'s turn. Which row?')
-	column = board[column_number]
-	for (cell, i) in enumerate(column):
-		if cell == 0:
-			column[i] = current_player
+	def switch_player(self):
+		if self.current_player == 1:
+			self.current_player = 2
+		else:
+			self.current_player = 1
+
+	def get_player_input(self):
+		column_number = raw_input('Player ' + str(self.current_player) + '\'s turn. Which row?')
+		column = self.board[column_number]
+		for (cell_value, row_number) in enumerate(column):
+			if cell_value == 0:
+				column[row_number] = self.current_player
+				break
+
+	def check_for_victory(self):
+		for column in self.board:
 			break
 
-def check_for_victory():
-	for column in board:
-		break
+	def display_board(self):
+		print self.board
 
-def display_board():
-	print board
+Connect4().setup().run()
